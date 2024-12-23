5fc430255f6f6f0790bf60f5e4aa2e8a2dbc6977
Redstone key <bennet-wegener@web.de>
2024-12-23 21:03:13 +0100

    Update zoomin.js

--- src/components/animations/zoomin.js
+++ src/components/animations/zoomin.js
@@ -7,9 +7,9 @@ import './animate.css';
 export default function Zoomin(){
     return (
         <div  className=''>
-            // eslint-disable-next-line
+
             <img src={Jeremy} height='400px'className='jeremy'alt="Description of "/>
-            // eslint-disable-next-line
+ 
             <img src={zoom} height='550px' className='zoom'alt="Description of "/>
 
         </div> 
